c0d85f3955f5f7b9703982981f2ec9646d3620e7
Utkarsh Singh <singhutkarsh841417@gmail.com>
2026-05-08 14:07:52 +0530

    implement Bubble Sort visualizer with animations and ViewModel integration

--- app/src/main/java/com/example/algovisualizer/presentation/auth/LoginScreen.kt
+++ app/src/main/java/com/example/algovisualizer/presentation/auth/LoginScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -18,7 +19,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.example.algovisualizer.presentation.common.UiState
 
@@ -35,7 +40,9 @@ fun LoginScreen(
     val state by viewModel.uiState.collectAsState()
 
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp)
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Login", style = MaterialTheme.typography.headlineMedium)
 
@@ -44,7 +51,8 @@ fun LoginScreen(
         OutlinedTextField(
             value = email,
             onValueChange = {email  = it},
-            label = {Text("Email")}
+            label = {Text("Email")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
         )
 
         Spacer(modifier = Modifier.height(8.dp))
@@ -52,7 +60,9 @@ fun LoginScreen(
         OutlinedTextField(
             value = password,
             onValueChange = {password = it},
-            label = {Text("Password")}
+            label = {Text("Password")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation()
         )
 
         Spacer(modifier = Modifier.height(16.dp))
